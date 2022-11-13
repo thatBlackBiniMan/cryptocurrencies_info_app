04ef38586c8145650fcfcccaf96a2a5e0e8e73bd
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-13 21:53:20 +0100

    still on Front-end

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from './components';  
+import './App.css';
+
+const App = () => {
+ return ( 
+  <div className="app">
+    <div className="navbar">
+    <Navbar/>
+    </div>
+    <div className="main">
+
+    </div>
+    <div className="footer">
+
+    </div>
+  </div>
+  );
+}
+
+
+ 
+export default App;
