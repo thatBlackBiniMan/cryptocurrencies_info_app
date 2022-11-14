8133908e2d1f0bd1a834e0465188ab72463d6840
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-14 10:55:55 +0100

    Still working on Homepage, ran into some errors, need a break

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from './components';  
+import { Navbar, Exachanges, Homepage, Cryptocurrencies, News, CryptoDetails} from './components';  
 import './App.css';
 
 const App = () => {
@@ -9,13 +9,42 @@ const App = () => {
   <div className="app">
     <div className="navbar">
     <Navbar/>
-    </div>
-    <div className="main">
+  </div>
+  
+  <div className="main">
+   <Layout>
+<div className="routes">
+  <Switch>
+    <Route exact path="/">
+    <Homepage/>
+    </Route>
+  
+    <Route exact path="/exachanges">
+    <Exachanges/>
+    </Route>
 
-    </div>
-    <div className="footer">
+    <Route exact path="/cryptocurrencies">
+   <Cryptocurrencies/>  
+    </Route>
 
-    </div>
+  
+    <Route exact path="/crypto/:coinId">
+   <CryptoDetails/>  
+    </Route>
+  
+ 
+    <Route exact path="/news">
+   <News/>  
+    </Route> 
+  </Switch>
+</div>
+   </Layout>
+  </div>
+
+  <div className="footer">
+
+  </div>
+  
   </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,14 +14,30 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">IGHO</Link>
         </Typography.Title>
-        <Menu theme="dark">
-          <Menu.Item icon={<HomeOutlined/>}/>
-          <Link to="/">Home</Link>
-        </Menu>
-        {/* <Button className="menu-control-container">
-
-        </Button> */}
       </div>
+     <Menu theme="dark">
+        
+           <Menu.Item icon={<HomeOutlined/>}>
+              <Link to="/">Home</Link>
+            </Menu.Item>
+          
+           <Menu.Item icon={<FundOutlined/>}>
+              <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+           </Menu.Item>
+          
+          
+          <Menu.Item icon={<MoneyCollectOutlined/>}>
+              <Link to="/exchanges">Exchanges</Link>
+            </Menu.Item>
+
+
+            <Menu.Item icon={<BulbOutlined/>}>
+              <Link to="/news">News</Link>
+            </Menu.Item>
+
+
+     </Menu>
+   
     </div>
    );
 }
