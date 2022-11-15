38528b6905c31fe31d2e28c567dbf3c79b93234a
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-15 09:02:19 +0100

    Using Redux and rapidApi(Fetching data)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
 import App from './App';
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import store from './app/store';
+
+
 
 ReactDOM.render(
   <Router>
-<App/>
+    <Provider store={store}>
+    <App/>
+    </Provider>
+
   </Router>
 , document.getElementById('root'));  
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,40 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+  'X-RapidAPI-Key': '0dc7bf0385msh98a8901abe94366p112bd3jsna99576616087', 
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/exchanges';
+
+
+const createRequest = (url) =>({url, headers: cryptoApiHeaders})
+
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl}),
+  endpoints: (builder) =>({
+      getCryptos: builder.query({
+        query: () => createRequest('/exchanges')
+      })
+  })
+});
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/exchanges',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     limit: '50',
+//     offset: '0',
+//     orderBy: '24hVolume',
+//     orderDirection: 'desc'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '0dc7bf0385msh98a8901abe94366p112bd3jsna99576616087',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//   }
+// }
+
