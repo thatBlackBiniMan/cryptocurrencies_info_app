2a4c055e78fa8854692fb8d94a1131defaf9d670
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-16 12:36:58 +0100

    Taking a Project break, Having problems fetching data & loading Ant Design

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,13 +5,33 @@ import { Card, Row, Col, Input} from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 
-const Cryptocurrencies = () => {
-  const {data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({simplified}) => {
+  const count = simplified ? 10 : 100;
+  const {data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const { cryptos, setCryptos} = useState(cryptosList?.data?.coins);
   
   console.log(cryptos);
+
+  if (isFetching) return 'Loading...';
+
   return ( 
-    <div>Cryptocurrencies</div>
+    <>
+    <Row gutter = {[32, 32]} className="crypto-card-container">
+      {cryptos?.map((currency)=>(
+        <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+          <Link to={`/crypto/${currency.id}`}>
+            <Card title={`${currency.rank}. ${currency.name}`} extra={<img className="crypto-image" alt="" src={currency.iconUrl}/>}
+            hoverable>
+            
+            <p>Price: {millify(currency.price)}</p>
+            <p>Market Cap: {millify(currency.marketCap)}</p>
+            <p>Daily Change: {millify(currency.change)}%</p>
+            </Card>
+          </Link>
+        </Col>
+      ))}
+    </Row>
+    </>
    );
 }
  
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -11,7 +11,7 @@ const { Title } = Typography;
 
 
 const Homepage = () => {
- const { data, isFetching } = useGetCryptosQuery();
+ const { data, isFetching } = useGetCryptosQuery(10);
  const globalStats = data?.data?.stats;
 
 
@@ -32,7 +32,7 @@ const Homepage = () => {
       <Title level={2} className="home-title">Top 10 Cryptocurrencies</Title>
       <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
     </div>
-<Cryptocurrencies simplified/>
+<Cryptocurrencies simplified />
 
 
 <div className="home-heading-container">
